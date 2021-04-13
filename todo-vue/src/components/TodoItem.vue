df8fce238b065359916d98add47a1dcd903eb455
Anton <anton.bazhan@pm.bet>
2021-04-13 12:51:43 +0300

    feat todo base

---
+++ todo-vue/src/components/TodoItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <li class="item-todo" >
+<span :class="{done:todo.completed}">
+      <input  type="checkbox" :checked="todo.completed">
+      <strong >{{ index + 1}}</strong>
+{{ todo.title }}
+      </span>
+      <button>&times;</button>
+    </li>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      id: Number,
+      title: String,
+      completed: Boolean
+    },
+    index:Number
+  }
+}
+
+</script>
+<style scoped>
+.item-todo{
+  list-style: none;
+  margin: 10px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-between;
+}
+
+.done{
+  text-decoration: line-through;
+}
+</style>
---
+++ todo-vue/src/components/Todos.vue
@@ -0,0 +1,71 @@
+<template>
+<div>
+  <h1>Todo app</h1>
+  <hr>
+  <form @submit.prevent="">
+    <input >
+    <button>Create todo</button>
+  </form>
+    <ul>
+<TodoItem
+    v-for="(todo,index) of todos"
+    :todo="todo"
+:index="index"/>
+  </ul>
+</div>
+</template>
+
+<script>
+import Vue from "vue";
+import TodoItem from "./TodoItem";
+export default {
+  components: {
+    TodoItem
+  },
+
+    data(){
+    return{
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        }
+        ]
+    }
+}
+}
+</script>
+
+
+<style scoped>
+.list-todo{
+
+}
+
+</style>
